f5d8a38352fc2430559c99e521a34fb980aff144
RBastos36 <r.bastos@ua.pt>
2025-01-27 15:22:30 +0000

    Updated cantilever dataset generation - Saves everything to a single HDF5 file

--- dataset-creation/dataset_generation_cantilever_beam.py
+++ dataset-creation/dataset_generation_cantilever_beam.py
@@ -1,6 +1,6 @@
 from topopt_cholmod_cantilever_beam import topopt
 import numpy as np
-import h5py
+
 
 nelx = 180
 nely = 60
@@ -14,30 +14,15 @@ num_load_position = 2
 volfrac = np.linspace(0.4, 0.5, num=num_volfrac)  # 7 evenly spaced values between 0.2 and 0.8
 load_position = np.linspace(0, 1, num=num_load_position)  # 11 evenly spaced values between 0 and 1
 
-# Create an HDF5 file to save data
-with h5py.File('topopt_results.h5', 'w') as hf:
-    # Create datasets for results
-    xPhys_dataset = hf.create_dataset('optimShape', shape=(num_volfrac, num_load_position, nely, nelx), dtype='float32')
-    obj_dataset = hf.create_dataset('obj', shape=(num_volfrac, num_load_position), dtype='float32')
+for i, vf in enumerate(volfrac):
+    for j, lp in enumerate(load_position):
+        load_config = {
+            'position': lp.item(),
+            'direction': 'vertical',
+            'magnitude': -1.0
+        }
+        # Call the topology optimization function
+        xPhys, obj = topopt(nelx, nely, vf.item(), penal, rmin, ft, load_config)
 
-    # Nested loop for volfrac and load_position
-    for i, vf in enumerate(volfrac):
-        for j, lp in enumerate(load_position):
-            load_config = {
-                'position': lp.item(),
-                'direction': 'vertical',
-                'magnitude': -1.0
-            }
-            # Call the topology optimization function
-            xPhys, obj = topopt(nelx, nely, vf.item(), penal, rmin, ft, load_config)
-
-            # Reshape or process xPhys if needed
-            xPhys_reshaped = xPhys.reshape((nelx, nely)).T
-
-            # TODO Fix xPhys saving to dataset / Check if the xPhys matrices are correct
-            # Save results into the HDF5 datasets
-            xPhys_dataset[i, j, :, :] = xPhys_reshaped
-            obj_dataset[i, j] = obj
-
-            # Print progress
-            print(f"Processed volfrac={vf.item()}, load_position={lp.item()}, Objective={obj}")
+        # Print progress
+        print(f"Processed volfrac={vf.item()}, load_position={lp.item()}, Objective={obj}")
