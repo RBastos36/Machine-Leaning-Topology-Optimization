bcb08c2dcea756e61607621edb63b2df96ce73be
Ricardo Bastos <r.bastos@ua.pt>
2025-03-12 00:21:45 +0000

    Small updates to dataset generation and saving scheme

--- dataset-creation/dataset_generation_cantilever_beam.py
+++ dataset-creation/dataset_generation_cantilever_beam.py
@@ -1,4 +1,4 @@
-from topopt_cholmod_cantilever_beam import topopt
+from topopt_cholmod_cantilever_beam_diagonal_load import topopt
 import numpy as np
 
 
@@ -8,21 +8,35 @@ rmin = 5.4
 penal = 3.0
 ft = 0  # ft==0 -> sens, ft==1 -> dens
 
-num_volfrac = 2
-num_load_position = 2
+num_volfrac = 7
+num_load_position = 11
+num_load_magnitude = 11
 
-volfrac = np.linspace(0.4, 0.5, num=num_volfrac)  # 7 evenly spaced values between 0.2 and 0.8
+volfrac = np.linspace(0.2, 0.8, num=num_volfrac)  # 7 evenly spaced values between 0.2 and 0.8
 load_position = np.linspace(0, 1, num=num_load_position)  # 11 evenly spaced values between 0 and 1
+load_magnitude_vertical = np.linspace(0, 100, num=num_load_magnitude)  # 10 evenly spaced values between 1 and 100
+load_magnitude_horizontal = np.linspace(0, 100, num=num_load_magnitude)  # 10 evenly spaced values between 1 and 100
 
 for i, vf in enumerate(volfrac):
     for j, lp in enumerate(load_position):
-        load_config = {
-            'position': lp.item(),
-            'direction': 'vertical',
-            'magnitude': -1.0
-        }
-        # Call the topology optimization function
-        xPhys, obj = topopt(nelx, nely, vf.item(), penal, rmin, ft, load_config)
+        for k, lmv in enumerate(load_magnitude_vertical):
+            for l, lmh in enumerate(load_magnitude_horizontal):
+                if lmh == 0 and lmv == 0:
+                    continue
+                else:
+                    # load_config = {
+                    #     'position': lp.item(),
+                    #     'direction': 'vertical',
+                    #     'magnitude': -lm.item()
+                    # }
+                    load_config = {
+                        'position': lp.item(),
+                        'horizontal_magnitude': lmh.item(),
+                        'vertical_magnitude': lmv.item()
+                    }
+                    # Call the topology optimization function
+                    xPhys, obj = topopt(nelx, nely, vf.item(), penal, rmin, ft, load_config)
 
         # Print progress
-        print(f"Processed volfrac={vf.item()}, load_position={lp.item()}, Objective={obj}")
+        print(f"Processed volfrac={vf.item()}, load_position={lp.item()}, 'horizontal_magnitude': {lmh.item()},"
+              f"'vertical_magnitude': {lmv.item()}, Objective={obj}")
